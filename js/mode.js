e19f1771ffde425b817c69add9e56fda29c61a67
Rustem <63965587+rustem-garr@users.noreply.github.com>
2024-05-19 23:12:45 +0500

    Update mode.js

--- js/mode.js
+++ js/mode.js
@@ -6,24 +6,17 @@ let user_info = document.getElementById("user-info")
 count = 1
 
 function changeMode() {  
-    if (source) {
-        video.removeChild(source);
-    }
-    let newSource = document.createElement('source');
-    newSource.type = 'video/mp4';
     if(count % 2 == 0){    
-        newSource.src  = "media/video/itcamp_dark.mp4";
+        source.src  = "media/video/itcamp_dark.mp4";
         card.style.backgroundColor = "rgb(52, 50, 50)";
         user_info.style.color="white";
         count+=1
         }
     else{
-        newSource.src = "media/video/itcamp_light.mp4";
+        source.src = "media/video/itcamp_light.mp4";
         card.style.backgroundColor = "rgb(206, 204, 204)";
         user_info.style.color="black";
         count+=1
     }
-
-    video.appendChild(newSource)
     video.load(); 
 }
